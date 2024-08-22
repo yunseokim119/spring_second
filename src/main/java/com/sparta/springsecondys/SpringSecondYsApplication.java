392f35d33e08144f4fc131591c50088b39d33c4d
Robbie <Robbie@email.com>
2024-08-22 19:58:16 +0900

    git init

---
+++ src/main/java/com/sparta/springsecondys/SpringSecondYsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springsecondys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondYsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecondYsApplication.class, args);
+    }
+
+}
